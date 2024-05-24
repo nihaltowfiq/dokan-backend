b87f2bcb265b35a5ed8bea6560e6737f53e13c4a
nihaltowfiq <nihaltowfiq@gmail.com>
2024-05-24 18:44:09 +0600

    main modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  controllers: [AppController],
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     UsersModule,
+    AuthModule,
+    ProductsModule,
+    OrdersModule,
+    CartModule,
   ],
 })
 export class AppModule {}
