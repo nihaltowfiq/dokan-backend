1fb71c7c3525690dca4d13c638c2af3f52dd1633
nihaltowfiq <nihaltowfiq@gmail.com>
2024-06-02 00:06:46 +0600

    products.module updated

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsController } from './products.controller';
+import { Product, ProductSchema } from './products.schema';
 import { ProductsService } from './products.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    JwtModule,
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
