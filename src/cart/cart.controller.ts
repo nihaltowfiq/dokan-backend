a3f17f43c1b81fbb9f49e81e0ec71cc5196cf008
nihaltowfiq <nihaltowfiq@gmail.com>
2024-06-04 23:50:01 +0600

    initial carts api's note added

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,4 +1,13 @@
 import { Controller } from '@nestjs/common';
 
+// { auth guard }
 @Controller('cart')
-export class CartController {}
+export class CartController {
+  // add to cart / update cart
+  // get cart
+  // remove from cart
+  // update product quantity
+  // get wishlist
+  // add to wishlist
+  // remove from wishlist
+}
