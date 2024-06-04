04a35dd5d459c632f6a14f12796ab3b4643af0d1
nihaltowfiq <nihaltowfiq@gmail.com>
2024-06-04 23:43:57 +0600

    initial product api's - notes added

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  // add single product { auth guard }
+  // get single product { no guard }
+  // get all products { no guard - pagination, filter, sort }
+  // update single product { auth guard }
+}
