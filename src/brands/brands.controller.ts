4086f0e3de18cb91a6d0f0cec328c7ad67a862e5
nihaltowfiq <nihaltowfiq@gmail.com>
2024-06-30 23:47:05 +0600

    listed brands module api's

--- src/brands/brands.controller.ts
+++ src/brands/brands.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('brands')
-export class BrandsController {}
+export class BrandsController {
+  // create brand { admin auth guard }
+  // update brand { admin auth guard }
+  // delete brand { admin auth guard }
+  // get all brands { admin auth guard - pagination, sorting, filter }
+}
--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -4,6 +4,7 @@ import { Controller } from '@nestjs/common';
 export class CategoriesController {
   // create category { admin auth guard }
   // update category { admin auth guard }
-  // get all categories { no guard }
   // delete categories { admin auth guard }
+  // get all categories { no guard - for customers }
+  // get all categories { no guard - for admin - pagination, sorting, filter }
 }
