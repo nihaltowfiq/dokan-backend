5f84b0020b22f70e38d1e15622c773767488599d
nihaltowfiq <nihaltowfiq@gmail.com>
2024-07-04 23:36:54 +0600

    brand schema added

--- src/brands/brands.schema.ts
+++ src/brands/brands.schema.ts
@@ -1,9 +1,18 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, HydratedDocument } from 'mongoose';
 
 export type BrandDocument = HydratedDocument<Brand>;
 
 @Schema({ collection: 'brands', timestamps: true })
-export class Brand extends Document {}
+export class Brand extends Document {
+  @Prop({ required: true, type: String })
+  name: string;
+
+  @Prop({ required: true, type: String, unique: true })
+  slug: string;
+
+  @Prop({ type: String })
+  logo: string;
+}
 
 export const BrandSchema = SchemaFactory.createForClass(Brand);
